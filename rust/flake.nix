c11476bc6f70d16c6fed0ec247f21126ef981ffe
DataHearth <dev@antoine-langlois.net>
2024-06-24 13:06:51 +0200

    rust: add missing nix packages

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,24 +1,22 @@
 {
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-      {
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rust-bin.stable.latest.default
+            openssl
+            pkg-config
           ];
         };
-      }
-    );
+      });
 }
