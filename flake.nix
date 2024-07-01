c3130ce3acadd3811c8095db797a13af3c0baa0f
DataHearth <dev@antoine-langlois.net>
2024-07-01 09:56:49 +0200

    node, sveltekit: add modules

--- flake.nix
+++ flake.nix
@@ -7,15 +7,22 @@
         path = ./python;
         description = "Basic python template";
       };
-
       rust = {
         path = ./rust;
         description = "Basic rust template";
       };
-
       go = {
         path = ./go;
         description = "Basic go template";
+      };
+      node = {
+        path = ./node;
+        description = "Basic NodeJS template";
+      };
+      sveltekit = {
+        path = ./sveltekit;
+        description =
+          "SvelteKit template with Typescript support and tooling enabled";
       };
     };
   };
